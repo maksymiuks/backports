4db89f0b43dd014b0e879f03dce01be54d76ff83
Szymon Maksymiuk <32574056+maksymiuks@users.noreply.github.com>
2024-10-14 15:01:56 +0200

    Update test_dotlibPaths.R

--- tests/test_dotlibPaths.R
+++ tests/test_dotlibPaths.R
@@ -1,4 +1,4 @@
-source("helper/helper.R")
+.source("helper/helper.R")
 
 f = get(".libPaths", envir = baseenv())
 expect_same = makeCompareFun(f, backports:::.libPaths)
@@ -15,4 +15,4 @@ if (getRversion() >= "4.1.0") {
   .libPaths(f, include.site = FALSE)
   expect_same()
 }
-.libPaths(save, include.site = length(.Library.site) && .Library.site %in% save)
+.libPaths(save, include.site = length(.Library.site) && any(.Library.site %in% save))
